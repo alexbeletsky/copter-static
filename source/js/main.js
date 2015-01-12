4acb542680d14735f9b7250936a2c7e57558fc4b
alexanderbeletsky <alexander.beletsky@gmail.com>
2015-01-12 06:27:39 +0100

    terminal added

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,29 @@
+var commands = {
+	'seismo demo': function (term) {
+
+	}
+};
+
+var unknowCommand = function (command, term) {
+	term.echo('\tcommand not found: ' + command + '\n');
+};
+
+var terminalHandler = function (command, term) {
+	var commandHandler = commands[command];
+
+	return commandHandler ? commandHandler(term) : unknowCommand(command, term);
+};
+
+var app = {
+	run: function () {
+		skrollr.init();
+		$('.terminal').terminal(terminalHandler, {
+			prompt: 'web.dev > ',
+			greetings: 'Welcome! Please type `seismo demo`\n\n'
+		});
+	}
+};
+
 $(function () {
-	skrollr.init();
+	app.run();
 });
