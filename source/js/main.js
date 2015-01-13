d0b46a17a276f59f4980ff49a17936de98d470fa
alexanderbeletsky <alexander.beletsky@gmail.com>
2015-01-13 07:04:00 +0100

    terminal enc

--- source/js/main.js
+++ source/js/main.js
@@ -1,56 +1,12 @@
-var script = [
-	{
-		command: 'll\n',
-		respond: 'current directory'
-	},
-	{
-		command: 'git status',
-		respond: 'no changes'
-	}
-];
-
-var player = {
-	play: function (script, term) {
-		term.insert('gi');
-
-		setTimeout(function () {
-			term.insert('t ');
-
-			setTimeout(function () {
-
-				term.insert('l');
-
-				setTimeout(function () {
-
-					term.insert('l\n');
-
-					setTimeout(function () {
-
-						term.echo('..');
-
-					}, 200);
-
-				}, 150);
-
-			}, 250);
-
-		}, 100);
-
-
-		// term.echo(function () {
-		// 	return 'sss';
-		// });
-
-		// setTimeout(function () {
-		// 	term.echo(script[0].respond);
-		// }, 500);
-		// term.set_command(script[0].command);
-	}
-};
-
 var commands = {
 	'demo': function (term) {
-		player.play(script, term);
+		term.echo('\nWelcome to demo!\
+\n\n\
+Seismo is able to deploy any static application, let\'s first \
+\n\
+create simple angular application.\
+\n\n\
+Please type `yo create`.\n');
 	}
 };
 
@@ -68,12 +24,12 @@ var app = {
 	run: function () {
 		skrollr.init();
 		$('.terminal').terminal(terminalHandler, {
-			prompt: 'web.dev > ',
+			prompt: '> ',
 			greetings: '\n \
- _____       _ \n \
-|   __| ___ |_| ___  _____  ___ \n \
-|__   || -_|| ||_ -||     || . | \n \
-|_____||___||_||___||_|_|_||___| \n \
+				 _____       _ \n \
+				|   __| ___ |_| ___  _____  ___ \n \
+				|__   || -_|| ||_ -||     || . | \n \
+				|_____||___||_||___||_|_|_||___| v.0.7 \n \
 \n\n \
 Welcome! Please type `demo`\n\n'
 		});
